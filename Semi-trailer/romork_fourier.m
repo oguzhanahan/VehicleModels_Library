758c25d6f3825485589e8700f011b29fb22cf891
oguzhanahan <50147568+oguzhanahan@users.noreply.github.com>
2023-04-15 23:04:45 +0300

    Add files via upload

---
+++ Semi-trailer/romork_fourier.m
@@ -0,0 +1,23 @@
+Ntr=length(t); %% number of points
+Ttr=100; %% define time of interval
+ttrr=[0:Ntr-1]/Ntr; %% define time
+ttrr=ttrr*Ttr; %% define time in seconds
+ftr=FTR; %%define function
+ptr=abs(fft(ftr))/(Ntr/2); %% absolute value of the fft
+ptr=ptr(1:Ntr/2).^2; %% take the power of positve freq. half
+freqtr=[0:Ntr/2-1]/Ttr; %% find the corresponding frequency in Hz
+plot(freqtr,ptr); %% plot on semilog scale
+%xlim([0 10]);
+
+fstr=length(t)/10;           % Sample frequency (Hz)
+tttrrr=0:1/fstr:10-1/fstr;     % Sample time (s)
+xtr=FTR;                    % Function
+mtr=length(xtr);               % Window length
+ntr=pow2(nextpow2(mtr));       % Transform length
+ytr=fft(xtr,ntr);              % DFT
+ftrtr=(0:ntr-1)*(fstr/ntr);      % Frequency range
+powertr=ytr.*conj(ytr)/ntr;    % Power of the DFT
+%plot(ftrtr,powertr)
+%xlabel('Frequency (Hz)')
+%ylabel('Power')
+xlim([0 100]);
